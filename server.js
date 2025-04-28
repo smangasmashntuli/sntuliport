3ff8187bb218e5fb86c04b91c7464a92f17283f1
Simangaliso Mazweni Ntuli <131978605+smangasmashntuli@users.noreply.github.com>
2025-04-28 22:31:18 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql2');
+const express = require ('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connection = mysql.createConnection({
+    host: 'localhost',  // Database host
+    user: 'root', // Database user
+    password: 'S!mangalis0',
+    database: 'mychatbot' // Database name
+});
+
+connection.connect(err => {
+    if(err){
+        console.error('Database connection error', err);
+        return;
+    }
+    console.log('Connected to database');
+});
+
+app.post('/submit', (req, res) => {
+    const {name, email, feedback} = req.body;
+    const sql = 'INSERT INTO user_datails (name, email, feedback) VALUES (?, ?, ?)';
+    connection.query(sql, [name, email, feedback], (err, result) => {
+        if (err){
+            console.error('Insert error:', err);
+            return res.status(500).send('Database error');
+        }
+        res.send('Data saved');
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
